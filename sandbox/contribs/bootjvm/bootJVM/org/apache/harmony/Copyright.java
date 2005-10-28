2520bbf33997d6f4d41c5172a723c59ee3259d01
Daniel Lydick <dlydick@apache.org>
2005-10-28 08:34:30 +0000

    Added strings to support 'getsvndata.sh', etc.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/trunk@329156 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/contribs/bootjvm/bootJVM/org/apache/harmony/Copyright.java
@@ -0,0 +1,71 @@
+/*!
+ * @file Copyright.java
+ *
+ * @brief Copyright text for Apache Harmony project.
+ *
+ * Contains the copyright text needed for all Java programs.
+ *
+ *
+ * @section Control
+ *
+ * \$URL: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/trunk/sandbox/contribs/bootjvm/bootJVM/org/apache/harmony/Copyright.java $
+ *
+ * \$Id: Copyright.java 326505 2005-10-19 09:35:33Z dlydick $
+ *
+ * Copyright 2005 The Apache Software Foundation
+ * or its licensors, as applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 ("the License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * @version \$LastChangedRevision: 326505 $
+ *
+ * @date \$LastChangedDate: 2005-10-19 04:35:33 -0500 (Wed, 19 Oct 2005) $
+ *
+ * @author Apache Software Foundation \$LastChangedBy: dlydick $
+ *
+ * @section Reference
+ *
+ */
+package org.apache.harmony;
+
+/*!
+ * @brief Copyright message for this file @e itself, meaningless
+ * outside of the context of this file.
+ *
+ */
+class localPackageVisibleCopyright
+{
+    static final String copyright =
+"\0$URL: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/trunk/sandbox/contribs/bootjvm/bootJVM/org/apache/harmony/Copyright.java $ " +
+"$Id: Copyright.java 326505 2005-10-19 09:35:33Z dlydick $ " +
+        Copyright.copyrightText;
+
+} /* END of localPackageVisibleCopyright */
+
+
+/*!
+ * @brief Copyright text, Java edition
+ *
+ */
+public interface Copyright
+{
+    public static final String copyrightText =
+"Copyright 2005 The Apache Software Foundation or its licensors, as applicable.";
+
+
+} /* END of Copyright */
+
+
+/* EOF */
