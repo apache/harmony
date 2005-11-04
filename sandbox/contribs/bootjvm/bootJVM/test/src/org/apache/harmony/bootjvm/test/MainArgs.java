c0d00716187b4de807b637f82fba50316a746663
Daniel Lydick <dlydick@apache.org>
2005-11-04 20:32:53 +0000

    Adjusted package structure to reflect org.apache.harmony organization.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/trunk@330887 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/contribs/bootjvm/bootJVM/test/src/org/apache/harmony/bootjvm/test/MainArgs.java
@@ -0,0 +1,71 @@
+/*!
+ * @file MainArgs.java
+ *
+ * @brief Test class for checking args[] array parameter to main()
+ *
+ *
+ * @section Control
+ *
+ * \$URL$
+ *
+ * \$Id$
+ *
+ * Copyright 2005 The Apache Software Foundation
+ * or its licensors, as applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 ("the License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * @version \$LastChangedRevision$
+ *
+ * @date \$LastChangedDate$
+ *
+ * @author \$LastChangedBy$
+ *
+ *         Original code contributed by Daniel Lydick on 09/28/2005.
+ *
+ * @section Reference
+ *
+ */
+
+package org.apache.harmony.bootjvm.test;
+
+import org.apache.harmony.Copyright.*;
+
+/*!
+ * @brief Test ability of main() to recognize args[] array.
+ *
+ */
+public class MainArgs
+{
+    /* Please see 'jvm/include/arch.h' for corresponding 'C' defns */
+    private static final String copyright =
+"\0$URL$ " +
+"$Id$ " +
+        org.apache.harmony.Copyright.copyrightText;
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("args[0] = /" + args[0] +
+                         "/ args[1] = /" + args[1] +
+                         "/ args[2] = /" + args[2] +
+                         "/ args[3] = /" + args[3]);
+        System.exit(3);
+    }
+
+} /* END of org.apache.harmony.bootjvm.test.MainArgs */
+
+
+/* EOF */
