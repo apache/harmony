ef4e2408f9c837b7a39c9f183af2d624f5b4eac3
David Tanzer <dtanzer@apache.org>
2005-12-08 15:17:05 +0000

    I added an entry for OS-X. I simply copied the function from FreeBSD. Also I'm not sure if the define I'm checking is the right one (__APPLE__).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/trunk@355121 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contribs/jchevm/jchevm/libjc/os_functions.c
+++ sandbox/contribs/jchevm/jchevm/libjc/os_functions.c
@@ -15,7 +15,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- * $Id: os_functions.c,v 1.1.1.1 2004/02/20 05:15:42 archiecobbs Exp $
+ * $Id$
  */
 
 #include "libjc.h"
@@ -38,6 +38,29 @@ The following functions must be defined here:
  ************************************************************************/
 
 #if defined(__FreeBSD__)
+
+#include <sys/sysctl.h>
+
+int
+_jc_num_cpus(_jc_env *env)
+{
+	static const char *const node = "hw.ncpu";
+	_jc_jvm *const vm = env->vm;
+	int num;
+
+        if (sysctlbyname(node, NULL, 0, &num, sizeof(num)) == -1) {
+		_jc_eprintf(vm, "sysctl(%s): %s", node, strerror(errno));
+		return 1;
+	}
+	return num;
+}
+
+/************************************************************************
+ *				OS X					*
+ ************************************************************************/
+
+/** @todo is this really the right way to detect OS X? */
+#elif defined(__APPLE__)
 
 #include <sys/sysctl.h>
 
