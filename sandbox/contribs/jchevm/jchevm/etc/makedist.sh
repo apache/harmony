4916e0e4f114e398616e8ddabbd1f11de2260f69
Archie Cobbs <archie@apache.org>
2006-01-09 23:24:33 +0000

    Slightly prettify and clean up.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/trunk@367426 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contribs/jchevm/jchevm/etc/makedist.sh
+++ sandbox/contribs/jchevm/jchevm/etc/makedist.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # $Id$
 
+# Bail on errors
+set -e
+
+# Parse command line
 CLASSPATH_HOME=/usr/local/classpath
 for ARG in $@; do
   case $ARG in
@@ -10,21 +14,31 @@ for ARG in $@; do
   esac
 done
 
+# Check directory
 if [ ! -f etc/makedist.sh ]; then
     echo '*** Error: run me from the top level directory please'
     exit 1
 fi
 
+# Check classpath is there
 GLIBJ="${CLASSPATH_HOME}/share/classpath/glibj.zip"
 if [ ! -f "${GLIBJ}" ]; then
     echo "*** Error: build and install classpath first (${GLIBJ} not found)"
     exit 1
 fi
 
+# Remove generated files
 rm -f java/jc.zip java/api.tgz
 
+# Configure (so we have Makefiles)
 sh etc/regen.sh ${1+"$@"}
-(cd java && make) || exit 1
-(cd include && make) || exit 1
+
+# Pre-build jc.zip so builder doesn't have to
+( cd java && make )
+
+# Create the distribution
 make dist
+
+# Clean up
+make distclean
 
