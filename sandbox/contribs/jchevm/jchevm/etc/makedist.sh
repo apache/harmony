ab3761d08979544f9a4c5c3e6716d2d4f49fc6b6
Archie Cobbs <archie@apache.org>
2006-01-09 22:36:50 +0000

    Generate jcjavah header files at build time instead of at distribution creation time, and use the 'jcjavah' tool from the build tree instead of the installed version. This has a few benefits: - The generated headers correspond to the version of Classpath actually   installed instead of the version present when the distribution was made. - It simplifies etc/makedist.sh and eliminates the need to run it as root. - It makes the distribution tarball slightly smaller.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/trunk@367421 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/contribs/jchevm/jchevm/etc/makedist.sh
+++ sandbox/contribs/jchevm/jchevm/etc/makedist.sh
@@ -11,26 +11,20 @@ for ARG in $@; do
 done
 
 if [ ! -f etc/makedist.sh ]; then
-    echo '***' run me from the top level directory please
+    echo '*** Error: run me from the top level directory please'
     exit 1
 fi
 
-if [ `id -u` -ne 0 ]; then
-    echo '***' you must be root
-    exit 1
-fi
-
-if [ ! -w ${CLASSPATH_HOME}/share/classpath/glibj.zip ]; then
-    echo '***' you must build and install classpath first
+GLIBJ="${CLASSPATH_HOME}/share/classpath/glibj.zip"
+if [ ! -f "${GLIBJ}" ]; then
+    echo "*** Error: build and install classpath first (${GLIBJ} not found)"
     exit 1
 fi
 
 rm -f java/jc.zip java/api.tgz
 
 sh etc/regen.sh ${1+"$@"}
-(cd tools && make && make install) || exit 1
-(cd java && make && make install) || exit 1
-(cd include && make && make install) || exit 1
-(cd libjc/native && make hfiles) || exit 1
+(cd java && make) || exit 1
+(cd include && make) || exit 1
 make dist
 
