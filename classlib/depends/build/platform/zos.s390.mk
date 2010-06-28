7e3454fa1162a65950c2839a60f990e30c200088
Mark Hindess <hindessm@apache.org>
2010-06-28 07:57:53 +0000

    Use the AREXTRACT make variable and override it on z/OS.  Thanks to Jim Yu for pointing out that z/OS build was broken.
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/java/trunk@958493 13f79535-47bb-0310-9956-ffa450edef68

--- classlib/depends/build/platform/zos.s390.mk
+++ classlib/depends/build/platform/zos.s390.mk
@@ -52,3 +52,6 @@ HYDEBUGCFLAGS = -g -O0
 # On z/OS set DLLPATH to LIBPATH so we link against .x export files in
 # $(HY_HDK)/lib instead of directly against the .so libraries.
 DLLPATH=$(LIBPATH)
+
+# AR doesn't support the 'o' option on z/OS so mtime will be updated
+AREXTRACT= x
