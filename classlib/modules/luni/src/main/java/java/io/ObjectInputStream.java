c840039961a4e117e6b7046bc7ae923fa588e207
Mark Hindess <hindessm@apache.org>
2010-08-11 21:13:45 +0000

    Merge change from /harmony/enhanced/java/trunk@984367:
      r984367 | hindessm | 2010-08-11 11:51:26 +0100 (Wed, 11 Aug 2010) | 3 lines
      
      Add test and fix for "[#HARMONY-6614] ObjectInputStream's primitive mapping
      table misses void/void.class".
      
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/java/branches/java6@984583 13f79535-47bb-0310-9956-ffa450edef68

--- classlib/modules/luni/src/main/java/java/io/ObjectInputStream.java
+++ classlib/modules/luni/src/main/java/java/io/ObjectInputStream.java
@@ -117,6 +117,7 @@ public class ObjectInputStream extends InputStream implements ObjectInput,
         PRIMITIVE_CLASSES.put("char", char.class); //$NON-NLS-1$
         PRIMITIVE_CLASSES.put("float", float.class); //$NON-NLS-1$
         PRIMITIVE_CLASSES.put("double", double.class); //$NON-NLS-1$
+        PRIMITIVE_CLASSES.put("void", void.class); //$NON-NLS-1$
     }
 
     private ObjectAccessor accessor = AccessorFactory.getObjectAccessor();
