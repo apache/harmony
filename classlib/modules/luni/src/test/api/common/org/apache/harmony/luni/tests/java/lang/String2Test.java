bcc7cda65926f6bbfdf261cd69707b07221b16ac
Tim Ellison <tellison@apache.org>
2010-09-24 04:14:11 +0000

    Re-apply fix for HARMONY-6649 (String.toLowerCase/toUpperCase incorrect for supplementary characters)
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/java/trunk@1000700 13f79535-47bb-0310-9956-ffa450edef68

--- classlib/modules/luni/src/test/api/common/org/apache/harmony/luni/tests/java/lang/String2Test.java
+++ classlib/modules/luni/src/test/api/common/org/apache/harmony/luni/tests/java/lang/String2Test.java
@@ -793,11 +793,14 @@ public class String2Test extends junit.framework.TestCase {
                 .toLowerCase().equals(hwlc));
 
         assertEquals(
-                "a) Sigma has same lower case value at end of word with Unicode 3.0",
+                "a) Sigma has ordinary lower case value when isolated with Unicode 4.0",
                 "\u03c3", "\u03a3".toLowerCase());
         assertEquals(
-                "b) Sigma has same lower case value at end of word with Unicode 3.0",
-                "a \u03c3", "a \u03a3".toLowerCase());
+                "b) Sigma has final form lower case value at end of word with Unicode 4.0",
+                "a\u03c2", "a\u03a3".toLowerCase());
+        
+        assertEquals("toLowerCase case conversion did not succeed",
+        		"\uD801\uDC44", "\uD801\uDC1C".toLowerCase());
     }
 
     /**
@@ -835,6 +838,8 @@ public class String2Test extends junit.framework.TestCase {
         String s = "a\u00df\u1f56";
         assertTrue("Invalid conversion", !s.toUpperCase().equals(s));
 
+        assertEquals("toUpperCase case conversion did not succeed",
+        		"\uD801\uDC1C", "\uD801\uDC44".toUpperCase());
     }
 
     /**
