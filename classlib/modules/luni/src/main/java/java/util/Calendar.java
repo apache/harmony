d1ad6295170012ba5494a03fe918e40ac614f3ec
Tim Ellison <tellison@apache.org>
2010-09-27 13:57:45 +0000

    Replace some NLS trailing comments with more readable suppress warnings annotation.
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/java/trunk@1001745 13f79535-47bb-0310-9956-ffa450edef68

--- classlib/modules/luni/src/main/java/java/util/Calendar.java
+++ classlib/modules/luni/src/main/java/java/util/Calendar.java
@@ -654,11 +654,12 @@ public abstract class Calendar implements Serializable, Cloneable,
      */
     public static final int PM = 1;
 
-    private static String[] fieldNames = { "ERA=", "YEAR=", "MONTH=", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-            "WEEK_OF_YEAR=", "WEEK_OF_MONTH=", "DAY_OF_MONTH=", "DAY_OF_YEAR=", //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-            "DAY_OF_WEEK=", "DAY_OF_WEEK_IN_MONTH=", "AM_PM=", "HOUR=", //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$ //$NON-NLS-4$
-            "HOUR_OF_DAY", "MINUTE=", "SECOND=", "MILLISECOND=", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-            "ZONE_OFFSET=", "DST_OFFSET=" }; //$NON-NLS-1$ //$NON-NLS-2$
+    @SuppressWarnings("nls")
+    private static String[] fieldNames = { "ERA=", "YEAR=", "MONTH=",
+            "WEEK_OF_YEAR=", "WEEK_OF_MONTH=", "DAY_OF_MONTH=", "DAY_OF_YEAR=",
+            "DAY_OF_WEEK=", "DAY_OF_WEEK_IN_MONTH=", "AM_PM=", "HOUR=",
+            "HOUR_OF_DAY", "MINUTE=", "SECOND=", "MILLISECOND=",
+            "ZONE_OFFSET=", "DST_OFFSET=" };
 
     /**
      * Constructs a {@code Calendar} instance using the default {@code TimeZone} and {@code Locale}.
