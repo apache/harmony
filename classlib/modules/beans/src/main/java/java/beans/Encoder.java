2a715e79e226bde440ef0d6c22637bffca3800df
Mark Hindess <hindessm@apache.org>
2010-09-29 17:03:34 +0000

    Fix typo spotted when doing merging.
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/java/trunk@1002736 13f79535-47bb-0310-9956-ffa450edef68

--- classlib/modules/beans/src/main/java/java/beans/Encoder.java
+++ classlib/modules/beans/src/main/java/java/beans/Encoder.java
@@ -260,7 +260,7 @@ public class Encoder {
     }
 
     /**
-     * Remvoe the existing new copy of the given old object.
+     * Remove the existing new copy of the given old object.
      * 
      * @param old
      *            an old object
