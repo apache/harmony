763b7ea45bb14bf7117ad75f4e696924235ccc43
Jesse Wilson <jessewilson@apache.org>
2010-10-04 22:59:23 +0000

    Isolate logging behavior tests from logging+security tests. Necessary to run the tests in a VM where installing an alternate security manager is not allowed.
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/java/trunk@1004471 13f79535-47bb-0310-9956-ffa450edef68

--- classlib/modules/logging/src/test/java/org/apache/harmony/logging/tests/java/util/logging/HandlerTest.java
+++ classlib/modules/logging/src/test/java/org/apache/harmony/logging/tests/java/util/logging/HandlerTest.java
@@ -497,23 +497,25 @@ public class HandlerTest extends TestCase {
 	public void testReportError() {
 		MockHandler h = new MockHandler();
 		h.setErrorManager(new MockErrorManager());
-		SecurityManager oldMan = System.getSecurityManager();
-		System.setSecurityManager(new MockSecurityManager());
 
+        Exception ex = new Exception("test exception");
+        // with non-null parameters
+        h.reportError("test msg", ex, -1);
+        assertEquals(-1, CallVerificationStack.getInstance().popInt());
+        assertSame(ex, CallVerificationStack.getInstance().pop());
+        assertEquals("test msg", CallVerificationStack.getInstance().pop());
+        // with null parameters
+        h.reportError(null, null, 0);
+        assertEquals(0, CallVerificationStack.getInstance().popInt());
+        assertSame(null, CallVerificationStack.getInstance().pop());
+        assertNull(CallVerificationStack.getInstance().pop());
+	}
+
+	public void testReportErrorWithSecurityManager() {
+        SecurityManager oldMan = System.getSecurityManager();
+		System.setSecurityManager(new MockSecurityManager());
 		try {
-			Exception ex = new Exception("test exception");
-			// with non-null parameters
-			h.reportError("test msg", ex, -1);
-			assertEquals(-1, CallVerificationStack.getInstance().popInt());
-			assertSame(ex, CallVerificationStack.getInstance().pop());
-			assertEquals("test msg", CallVerificationStack.getInstance().pop());
-			// with null parameters
-			h.reportError(null, null, 0);
-			assertEquals(0, CallVerificationStack.getInstance().popInt());
-			assertSame(null, CallVerificationStack.getInstance().pop());
-			assertNull(CallVerificationStack.getInstance().pop());
-		} catch (SecurityException e) {
-			fail("Should not throw SecurityException!");
+            testReportError();
 		} finally {
 			System.setSecurityManager(oldMan);
 		}
