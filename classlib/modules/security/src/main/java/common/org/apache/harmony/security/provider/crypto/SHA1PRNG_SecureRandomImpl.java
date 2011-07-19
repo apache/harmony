724deb045a85b722c961d8b5a83ac7a697319441
Mark Hindess <hindessm@apache.org>
2011-07-19 20:18:07 +0000

    Merge change from /harmony/enhanced/java/trunk@1148459:
      r1148459 | hindessm | 2011-07-19 18:38:16 +0100 (Tue, 19 Jul 2011) | 1 line
      
      Apply patch from HARMONY-6685 "SHA1PRNG produces less optimal"
    
    
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/java/branches/java6@1148509 13f79535-47bb-0310-9956-ffa450edef68

--- classlib/modules/security/src/main/java/common/org/apache/harmony/security/provider/crypto/SHA1PRNG_SecureRandomImpl.java
+++ classlib/modules/security/src/main/java/common/org/apache/harmony/security/provider/crypto/SHA1PRNG_SecureRandomImpl.java
@@ -317,7 +317,7 @@ public class SHA1PRNG_SecureRandomImpl extends SecureRandomSpi implements
                 seed[i] = 0;
             }
 
-            bits = seedLength << 3 + 64; // transforming # of bytes into # of bits
+            bits = (seedLength << 3) + 64; // transforming # of bytes into # of bits
 
             // putting # of bits into two last words (14,15) of 16 word frame in 
             // seed or copies array depending on total length after padding
